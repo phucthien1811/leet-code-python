b47f9478a5d91c2fd3cdee526363e24969aa1ebc
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-02 15:00:53 +0700

    hash map time o(n)

--- twoSum.py
+++ twoSum.py
@@ -2,14 +2,23 @@ from typing import List
 
 
 class Solution:
+    #brute force
+    #Time complexity: O(n^2)
+    #memory complexity: O(1)
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-                
+      #  for i in range(len(nums)):
+        #    for j in range(i + 1, len(nums)):
+          #      if nums[i] + nums[j] == target:
+        #            return [i, j]
+           #     
 
+       # return []
+        visit = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in visit:
+                return [visit[diff], i]     
+            visit[num] = i
         return []
-    
 print(Solution().twoSum([2, 7, 11, 15], 9))
 
