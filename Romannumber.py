d66f85ba15f6e009d433b012a3d94b9f51ad7704
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-03 19:36:09 +0700

    toi uu siieu nhanh c3

--- Romannumber.py
+++ Romannumber.py
@@ -1,13 +1,20 @@
 def romanToInt(s: str) -> int:
-    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-             'C': 100, 'D': 500, 'M': 1000}
+    vals = [0] * 26
+    vals[ord('I') - 65] = 1
+    vals[ord('V') - 65] = 5
+    vals[ord('X') - 65] = 10
+    vals[ord('L') - 65] = 50
+    vals[ord('C') - 65] = 100
+    vals[ord('D') - 65] = 500
+    vals[ord('M') - 65] = 1000
+
     total = prev = 0
     for c in reversed(s):
-        val = roman[c]
+        val = vals[ord(c) - 65]
         if val < prev:
             total -= val
         else:
             total += val
         prev = val
     return total
-print(romanToInt("III"))  # Output: 3
+print(romanToInt("MCMXCIV"))  # Output: 1994
