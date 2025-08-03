2ae1be8aa3d562f69227be66d7e85c2954248e36
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-03 18:49:42 +0700

    tu phai sang trai cacch 2

--- Romannumber.py
+++ Romannumber.py
@@ -1,13 +1,13 @@
 def romanToInt(s: str) -> int:
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
              'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    for i in range(len(s)):
-        val = roman[s[i]]
-        if i + 1 < len(s) and val < roman[s[i + 1]]:
+    total = prev = 0
+    for c in reversed(s):
+        val = roman[c]
+        if val < prev:
             total -= val
         else:
             total += val
+        prev = val
     return total
-
-print(romanToInt("MCMXCIV"))  
+print(romanToInt("III"))  # Output: 3
