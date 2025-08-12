29e5289295a6ce975d842a5ec30f6e4855f4aa45
Thien Tiy <jack0baohbocon@gmail.com>
2025-08-12 19:49:41 +0700

    cách đệ quy

--- Mergen2ShortList2.py
+++ Mergen2ShortList2.py
@@ -1,3 +1,9 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def mergeTwoLists(self, list1: ListNode, list2: ListNode) -> ListNode:
         if not list1:
